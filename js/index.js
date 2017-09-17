3ee174cc2600b325f5d5cd7529609f526b66ea9d
Armanit Garg <armanit.garg@gmail.com>
2017-09-17 08:02:14 -0400

    Added authentication functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,72 @@
 var mainApp = angular.module('mainApp',[]);
 
+var errorDebug = {
+    userAuthenticationError: null,
+}
+
+var dataTransfer = {
+    
+    loadData : function(nodeId , callback) {
+        firebase.database().ref('treeData/' + nodeId).once('value').then(function(snapshot) {
+            callback(snapshot.val());
+        });
+    }, 
+
+    addData : function(newNode){
+        firebase.database().ref('treeData/' + newNode.nodeId).set(newNode);        
+    }
+
+};
+
+var userAuthentication = {
+
+    login : function(email,password){
+        firebase.auth().signInWithEmailAndPassword(email,password).catch(function(error) {
+			errorDebug.userAuthenticationError = error;
+        });
+    },
+
+    logout : function(callback){
+        firebase.auth().signOut().then(callback).catch(function(error) {
+            errorDebug.userAuthenticationError = error;
+        });
+    },
+
+    createNewUser : function(email, password){
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            errorDebug.userAuthenticationError = error;
+        });
+    }
+
+};
+
+var Node = function(title, nodeStory, nodeId, parentId, choices) {
+    this.title = title; // The title of this chapter/section
+    this.nodeStory = nodeStory; // The story, an array (for linebreaks)
+    this.nodeId = nodeId;   // The id of the node in the tree
+    this.parentId = parentId;   // The parent node's id
+    this.choices = choices; // The choices at the end of this section, an array
+    this.rating = 0; // rating of section is initialized to 0 by default
+}
+
+var nodeNav = {
+    
+    getNode : function(nodeId , callback){
+        return dataTransfer.loadData(nodeId, callback);
+    },
+    
+    createNode : function(nodeStory, parentId, childNum, numberOfChildren){
+        var nodeId = parentId + '_' + childNum;
+        var newNode = new Node(nodeStory, nodeId, parentId, numberOfChildren);
+        dataTransfer.addData(newNode);
+    },
+
+    nextNode : function(currentNode, childIndex, callback) {
+        nodeNav.getNode(currentNode + '_' + childIndex, callback);
+    }
+
+};
+
 mainApp.directive('dynamic', function ($compile) {
   return {
     restrict: 'A',
@@ -15,18 +82,62 @@ mainApp.directive('dynamic', function ($compile) {
 
 mainApp.controller('MainCtrl', ['$http', '$scope', '$sce', function($http, $scope, $sce) {
 
+	$scope.ifUserLoggedIn = false;
 	$scope.currentSection;
-	$scope.currentSuggestions = ['lmao', 'cool', 'nice'];
-	$scope.currentPageNumber = 1;
-	$scope.currentRating;
-	$scope.storyContent = 'A narrative or story is a report of connected events, real or imaginary, presented in a sequence of written or spoken words, or still or moving images,[1][2] or both. The word derives from the Latin verb narrare, "to tell", which is derived from the adjective gnarus, "knowing" or "skilled".[3] Narrative can be organized in a number of thematic or formal categories: non-fiction (such as definitively including creative non-fiction, biography, journalism, transcript poetry, and historiography); fictionalization of historical events (such as anecdote, myth, legend, and historical fiction); and fiction proper (such as literature in prose and sometimes poetry, such as short stories, novels, and narrative poems and songs, and imaginary narratives as portrayed in other textual forms, games, or live or recorded performances).';
 	$scope.currentPageNumber = 0;
 	$scope.currentRating = 0;
 	$scope.choices;
+	//$scope.storyTitle = "Sample Title";
 	$scope.storyTitle;
+	//$scope.storyContent = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit justo, varius vitae vulputate et, mattis ac orci. Phasellus ac odio ut dui lacinia suscipit a id dolor. Vestibulum at vestibulum nunc. Vestibulum tincidunt lectus ante, sit amet mollis dui iaculis a. Vestibulum tincidunt, neque sit amet porta viverra, sapien lacus congue neque, non ultrices neque mauris eget odio. Cras interdum ornare lectus, eu cursus nisi gravida ut. In sit amet neque nec arcu convallis vehicula nec id felis. Aliquam erat volutpat. Maecenas ornare odio vel odio fermentum, a lobortis turpis tincidunt. Quisque facilisis enim ut tincidunt hendrerit. Pellentesque rhoncus nulla vel felis vulputate, sed egestas lorem ultrices.Cras non felis massa. Sed ut ligula congue, venenatis sapien quis, finibus libero. Nullam eu massa orci. Pellentesque bibendum, nisi non cursus facilisis, justo nunc pretium nulla, at auctor libero ipsum fringilla nisi. Pellentesque sagittis mi sagittis, accumsan enim eget, lacinia leo. Nulla vitae sollicitudin leo, porttitor vehicula sapien. Suspendisse potenti. Ut aliquam facilisis arcu vel tristique. Phasellus pretium gravida felis, eu vulputate eros gravida quis. Fusce elit augue, rhoncus non mauris eget, fringilla congue sem. Cras porttitor malesuada magna nec pulvinar. Nulla ornare maximus mollis. Donec fringilla finibus iaculis. Morbi bibendum lorem et elit fermentum, vitae cursus leo aliquet. Ut id arcu egestas, mattis mi non, hendrerit lorem. Fusce vestibulum augue in dui cursus sollicitudin. Nullam fringilla maximus ipsum, aliquet dictum purus vehicula eu. Proin accumsan, lectus vel euismod commodo, arcu eros dictum mauris, a aliquam odio nibh sit amet sapien. Duis tincidunt diam at ipsum aliquet, at fermentum odio rhoncus. Fusce a egestas enim. Mauris ullamcorper ex in arcu posuere cursus.Cras eros ex, semper a mattis nec, eleifend eget nibh. Aenean non facilisis erat, in tempor mauris. Morbi vulputate, nunc in faucibus accumsan, massa mauris laoreet mi, eget aliquet erat dolor in lorem. Duis ultricies sapien aliquam nibh gravida lobortis in eu lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus tincidunt dolor id sem tempor, vitae aliquet ligula accumsan. Aliquam erat volutpat. Praesent nec pretium massa. Fusce efficitur purus at quam ultrices efficitur. Nam vel sapien quis nisi viverra rutrum. Vivamus eget diam eu nisl pharetra euismod.Curabitur vitae odio accumsan, finibus purus ut, eleifend arcu. Sed nisi libero, ullamcorper vitae justo vestibulum, dignissim vulputate velit. Integer elit lorem, gravida ut vestibulum ut, fringilla et lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis euismod ex eget enim placerat, vel finibus risus tempor. Sed diam nunc, laoreet at sodales in, tincidunt vel ex. Maecenas molestie varius ligula. Donec ac pretium ligula, eget accumsan leo. Cras in auctor lectus. Praesent et massa quis eros lacinia ornare eu a lectus.'];
 	$scope.storyContent;
+	$scope.fromLogin = true;
+
+	$scope.logout = function(){
+		userAuthentication.logout(function(){
+			$scope.ifUserLoggedIn = false;
+		})
+	};
 
 	angular.element(document).ready(function() {
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyBbMkxw2Enicr_MMN0CIKacBbo4Dzbh0iM",
+			authDomain: "thatonestory-39da0.firebaseapp.com",
+			databaseURL: "https://thatonestory-39da0.firebaseio.com",
+			projectId: "thatonestory-39da0",
+			storageBucket: "thatonestory-39da0.appspot.com",
+			messagingSenderId: "679043859868"
+		};
+		
+		firebase.initializeApp(config);
+
+		document.getElementById('loginButton').onclick = function(){
+			userAuthentication.login($scope.username, $scope.password);
+			$scope.fromLogin = true;
+		}
+
+		document.getElementById('signUpButton').onclick = function(){
+			userAuthentication.createNewUser($scope.usernameSignUp, $scope.passwordSignUp);
+			$scope.fromLogin = false;
+		}
+
+		firebase.auth().onAuthStateChanged(function(user) {
+			if (user) {
+				$scope.emailOfUser = user.email;
+				$scope.ifUserLoggedIn = true;
+				if($scope.fromLogin){
+					$scope.closeModal();
+				}else{
+					$scope.closeAddUserModal();
+				}
+				$scope.$apply();
+			} else {
+				// On Successful Logout
+				
+			}
+		});
+
 		var start = nodeNav.getNode('tree_0', function(node) {
 			$scope.$apply(function() {
 				$scope.currentSection = node.nodeId;
@@ -173,6 +284,14 @@ mainApp.controller('MainCtrl', ['$http', '$scope', '$sce', function($http, $scop
 
 	$scope.closeModal = function() {
 		document.getElementById('myModal').style.display = "none";
+	}
+
+	$scope.addUserModal = function() {
+		document.getElementById('addUser').style.display = "block";
+	}
+
+	$scope.closeAddUserModal = function() {
+		document.getElementById('addUser').style.display = "none";
 	}
 
 }]);
